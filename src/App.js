d1747b284fbf54fb7212896b0f5c55336222244b
Carlos Caro <carlos@gmail.com>
2022-08-22 20:31:47 +0200

    Contador de clicks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import logoMorado from './imagenes/imagen-logo.png';
 import './App.css';
+import Boton from './componentes/Boton';
+import './hojas de estilos/Boton.css';
+import Contador from './componentes/contador';
+import './hojas de estilos/Contador.css'
+import { useState } from 'react';
 
 function App() {
+
+  const [numClics, setNumClics] = useState(0); 
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  }
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='logo-contenedor'>
+        <img
+          className='logoMorado'
+          src={logoMorado}
+          alt="logo de carlos"
+        />
+
+      </div>
+
+      <div className='contenedor-principal'>
+        {/* Contador de botones */}
+        <Contador 
+          numClics = { numClics }/>
+
+        {/* Boton de sumar +1 */}
+        <Boton 
+          texto='click'
+          esBotonDeClic={true}
+          manejarClic= { manejarClic }/>
+        {/* Boton de reinicio */}
+        <Boton 
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic= { reiniciarContador }/>
+      </div>
+
     </div>
   );
 }
